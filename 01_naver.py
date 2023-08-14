5c4ab4acaa3cb2a1969c36eb6b849496995bb245
bbangjun <lucid9dawn@gmail.com>
2023-08-15 02:06:02 +0900

    feat(01_naver.py): User-Agent 설정 후 get() 요청 출ㄹ력

--- 01_naver.py
+++ 01_naver.py
@@ -3,14 +3,16 @@ from bs4 import BeautifulSoup # beautiful soup 라이브러리 import
 
 url = "https://naver.com"
 
-req = requests.get(url) # GET 방식으로 naver에 요청
+headers = {
+    "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+} # dictionary
+
+req = requests.get(url, headers = headers) # GET 방식으로 naver에 요청
 
 print(req.request.headers)
 # print(dir(req))
 
 html = req.text # 요청을 하여 html을 받아옴
-
-# Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36
 
 # print(html) # html 출력
 
