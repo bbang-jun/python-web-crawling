b5c74db8308be4ad0bf41ccfa73889723038b68e
bbangjun <lucid9dawn@gmail.com>
2023-08-15 01:39:52 +0900

    feat(01_naver.py): url을 요청한 디렉토리 안의 request 출력

--- 01_naver.py
+++ 01_naver.py
@@ -5,6 +5,17 @@ url = "https://naver.com"
 
 req = requests.get(url) # GET 방식으로 naver에 요청
 
+print(req.request)
+# print(dir(req))
+
 html = req.text # 요청을 하여 html을 받아옴
 
-print(html) # html 출력
+# Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36
+
+# print(html) # html 출력
+
+soup = BeautifulSoup(html, "html.parser") # html을 html.parser로 분석(클래스를 통한 객체 생성)
+
+# logo = soup.select_one(".logo_naver").text # class면 . id면 #
+
+# print(logo)
