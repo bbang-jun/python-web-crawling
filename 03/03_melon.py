dc5b2087e2503cc590455422ed27943ee4af4c6c
bbangjun <lucid9dawn@gmail.com>
2023-08-15 20:54:06 +0900

    feat(03_melon.py): enumerate()를 통한 순위 및 노래 제목 출력(개행 이슈 해결 전)

--- 03/03_melon.py
+++ 03/03_melon.py
@@ -11,4 +11,14 @@ req = requests.get(url, headers = headers) # GET 방식으로 naver에 요청
 # html = req.text # 요청을 하여 html을 받아옴
 html = req.content.decode('utf-8','replace') # 한글이 깨지는 현상 발생으로 utf-8로 변경
 
-print(html)
+soup = BeautifulSoup(html, "html.parser") # html을 html.parser로 분석(클래스를 통한 객체 생성)
+
+lst50 = soup.select(".lst50")
+lst100 = soup.select(".lst100")
+
+top100 = lst50 + lst100
+
+for rank, i in enumerate(top100, start=1):
+    title = i.select_one(".ellipsis.rank01").text
+    print(f"{rank} : {title}")
+    print()
