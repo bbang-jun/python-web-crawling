082ee29daf723f461dc32910f7c1022a2cb89f7c
bbangjun <lucid9dawn@gmail.com>
2023-08-18 16:37:20 +0900

    feat(06_1_ssg_event.py): ssg.com을 URL로설정

---
+++ 06/06_1_ssg_event.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup # beautiful soup 라이브러리 import
+
+# item_issue
+
+headers = {
+    "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+} # dictionary
+
+url = "https://www.ssg.com/event/eventMain.ssg"
+
+req = requests.get(url, headers = headers) # GET 방식으로 naver에 요청
+html = req.text # 요청을 하여 html을 받아옴
+
+soup = BeautifulSoup(html, "html.parser") # html을 html.parser로 분석(클래스를 통한 객체 생성)
