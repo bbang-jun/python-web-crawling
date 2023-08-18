f0b2674183bccb4968be028102e78e23255eac0c
bbangjun <lucid9dawn@gmail.com>
2023-08-18 15:46:29 +0900

    feat(05_daum_news.py): 언론사 파싱 완료

--- 05/05_daum_news.py
+++ 05/05_daum_news.py
@@ -7,11 +7,11 @@ headers = {
     "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
 } # dictionary
 
-url = "https://news.daum.net"
+url = "https://news.daum.net/"
 
 req = requests.get(url, headers = headers) # GET 방식으로 naver에 요청
 # html = req.text # 요청을 하여 html을 받아옴
-html = req.content.decode('utf-8','replace') # 한글이 깨지는 현상 발생으로 utf-8로 변경
+html = req.text # 한글이 깨지는 현상 발생으로 utf-8로 변경
 
 soup = BeautifulSoup(html, "html.parser") # html을 html.parser로 분석(클래스를 통한 객체 생성)
 
@@ -21,6 +21,6 @@ soup = BeautifulSoup(html, "html.parser") # html을 html.parser로 분석(클래
 item_issue = soup.select(".item_issue")
 
 for item in item_issue:
-    press = item.select(".thumb_g")[1]
+    press = item.select_one(".logo_cp > img")["alt"]
     print(press)
     print()
