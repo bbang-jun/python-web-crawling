0f2617e7fba485b863fbb6946b7198b5173cd006
bbangjun <lucid9dawn@gmail.com>
2023-08-18 13:11:41 +0900

    comment: 가수명 클릭했을 때 나오는 js 링크로부터 가수 고유 숫자 확인

--- 03/03_melon.py
+++ 03/03_melon.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup # beautiful soup 라이브러리 import
 
+# javascript:melon.link.goArtistDetail('725987'); -> js에서 숫자 추출
+
 headers = {
     "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
 } # dictionary
