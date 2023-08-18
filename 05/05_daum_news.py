621f661e7943b6b2ed6c4f09ec3ed6c213f5af59
bbangjun <lucid9dawn@gmail.com>
2023-08-18 14:56:18 +0900

    feat(05_daum_news.py): 다음 뉴스에서 공통된 가장 큰 class인 item_issue select

---
+++ 05/05_daum_news.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup # beautiful soup 라이브러리 import
+
+# item_issue
+
+headers = {
+    "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+} # dictionary
+
+url = "https://news.daum.net"
+
+req = requests.get(url, headers = headers) # GET 방식으로 naver에 요청
+# html = req.text # 요청을 하여 html을 받아옴
+html = req.content.decode('utf-8','replace') # 한글이 깨지는 현상 발생으로 utf-8로 변경
+
+soup = BeautifulSoup(html, "html.parser") # html을 html.parser로 분석(클래스를 통한 객체 생성)
+
+# title = soup.select_one(".item_issue.cont_thumb.tit_g")
+# print(title.text)
+
+item_issue = soup.select(".item_issue")
+
+for item in item_issue:
