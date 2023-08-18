087ce7abf553b973a13c962367a639ea6b11306b
bbangjun <lucid9dawn@gmail.com>
2023-08-19 01:15:49 +0900

    feat(07_1.cgv.py): 19개 영화 제목 추출 완료(html 파일 생성 및 소 스 복붙 후에 정렬해서 보면 편함. ctrl+f로 하나만 존재하면 select_one

--- 07/07_1_cgv.py
+++ 07/07_1_cgv.py
@@ -13,3 +13,14 @@ html = req.text
 
 soup = BeautifulSoup(html, "html.parser")
 
+# sect-movie-chart
+
+sect_movie_chart = soup.select_one(".sect-movie-chart")
+
+movie_chart = sect_movie_chart.select("li")
+
+print(len(movie_chart)) # 19개가 나왔으므로 movie_chart 안에서 반복을 돌리며 추출하면 됨
+
+for movie in movie_chart:
+    title = movie.select_one(".title")
+    print(title.text)
