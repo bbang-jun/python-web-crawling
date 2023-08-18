264b0aea3531d66a110c028c2aee06e90960524a
bbangjun <lucid9dawn@gmail.com>
2023-08-19 00:03:10 +0900

    feat(06_1_ssg_event.py): 이벤트 링크 파싱

--- 06/06_1_ssg_event.py
+++ 06/06_1_ssg_event.py
@@ -13,3 +13,18 @@ req = requests.get(url, headers = headers) # GET 방식으로 naver에 요청
 html = req.text # 요청을 하여 html을 받아옴
 
 soup = BeautifulSoup(html, "html.parser") # html을 html.parser로 분석(클래스를 통한 객체 생성)
+
+evt_osmu_lst = soup.select_one(".evt_osmu_lst")
+
+evt_osmu_unit = evt_osmu_lst.select(".eo_link")
+
+for unit in evt_osmu_unit:
+    link = unit["href"]
+    print(link)
+    print()
+
+# units = evt_osmu_lst.select(".eo_link")
+#
+# for unit in units:
+#     link = unit["href"]
+#     print(link)
