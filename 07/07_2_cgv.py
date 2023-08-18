133a01359b326cdc88450d5a3505c957cc6c4467
bbangjun <lucid9dawn@gmail.com>
2023-08-19 01:35:15 +0900

    feat(07_2_cgv.py): 바로 다음 요소를 가져오는 next_element 함수로 개봉일 뒤의 텍스트를 무시하고 개봉일만 크롤링

--- 07/07_2_cgv.py
+++ 07/07_2_cgv.py
@@ -15,9 +15,9 @@ soup = BeautifulSoup(html, "html.parser")
 
 # sect-movie-chart
 
-sect_movie_chart = soup.select_one(".sect-movie-chart")
+sect_movie_chart = soup.select_one(".sect-movie-chart") # 19개 전부 다 들어있는 하나의 덩어리
 
-movie_chart = sect_movie_chart.select("li")
+movie_chart = sect_movie_chart.select("li") # 각 영화
 
 print(len(movie_chart)) # 19개가 나왔으므로 movie_chart 안에서 반복을 돌리며 추출하면 됨
 
@@ -26,7 +26,10 @@ for movie in movie_chart:
     score = movie.select_one(".score") # 예매율 %와 선호도 %가 함께 들어 있는 class
     ticketing = score.select_one(".percent")
     egg_percent = score.select_one(".egg-gage.small > .percent")
+    info = movie.select_one(".txt-info > strong").next_element
+
     print(title.text)
     print(ticketing.get_text(" : "))
     print(egg_percent.text)
+    print(info.strip())
     print()
