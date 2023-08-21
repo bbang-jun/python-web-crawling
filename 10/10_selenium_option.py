aad2f5f1a28a5bbf57e0720512d45c2eee076d41
bbangjun <lucid9dawn@gmail.com>
2023-08-21 16:11:17 +0900

    feat(10_selenium_option.py): --headless 옵션을 통해 브라우저를 켜지 않고 작업 수행

--- 10/10_selenium_option.py
+++ 10/10_selenium_option.py
@@ -9,7 +9,7 @@ options.add_experimental_option("detach", True)
 # options.add_argument("--start-maximized")
 # options.add_argument("--start-fullscreen")
 options.add_argument("window-size=500,500")
-options.add_argument(f"user-agent={user_agent}")
+options.add_argument("--headless")
 
 driver = webdriver.Chrome(options = options)
 
