22ae4340a636edb1a50bff73abd1ba72300ae4e0
bbangjun <lucid9dawn@gmail.com>
2023-08-23 11:11:14 +0900

    feat(11_selenium_naver.py): 반복문으로 VIEW  탭에서 10번 스크롤 후 html 받아와서 정보 출력

--- 11/11_selenium_naver.py
+++ 11/11_selenium_naver.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 # enter와 같은 key를 사용하기 위한 라이브러리
 from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
 import time
 
 options = Options()
@@ -30,20 +31,44 @@ time.sleep(0.5)
 driver.find_element(By.XPATH, '//*[text()="VIEW"]').click()
 time.sleep(0.5)
 
-# clear()를 통해 검색창에 남아 있는 검색어 제거
-driver.find_element(By.NAME, "query").clear()
-time.sleep(0.5)
-
-driver.find_element(By.NAME, "query").send_keys("하입보이")
-time.sleep(0.5)
-
-# 처음에 검색 버튼을 클릭했던 방법 대신 enter 키를 입력하는 방법으로 검색
-driver.find_element(By.NAME, "query").send_keys(Keys.ENTER)
-time.sleep(0.5)
+# # clear()를 통해 검색창에 남아 있는 검색어 제거
+# driver.find_element(By.NAME, "query").clear()
+# time.sleep(0.5)
+#
+# driver.find_element(By.NAME, "query").send_keys("하입보이")
+# time.sleep(0.5)
+#
+# # 처음에 검색 버튼을 클릭했던 방법 대신 enter 키를 입력하는 방법으로 검색
+# driver.find_element(By.NAME, "query").send_keys(Keys.ENTER)
+# time.sleep(0.5)
 
 # html의 body 태그에 페이지를 아래로 내리도록 함
-driver.find_element(By.TAG_NAME, "body").send_keys(Keys.END)
-time.sleep(0.5)
+for i in range(10):
+    driver.find_element(By.TAG_NAME, "body").send_keys(Keys.END)
+    time.sleep(1)
 
-# 스크린샷 찍기
-driver.save_screenshot("screenshot/naver.png")
+# html을 받아오는 시점은 원하는 곳으로 이동을 완료한 후
+html = driver.page_source
+
+soup = BeautifulSoup(html, "html.parser")
+
+items = soup.select(".api_ani_send")
+
+rank_num = 1
+for area in items:
+    ad = area.select_one(".link_ad")
+    if ad:
+        continue
+
+    print(f"<<<{rank_num}>>>")
+
+    title = area.select_one(".api_txt_lines.total_tit")
+    name = area.select_one(".sub_txt.sub_name")
+    print(name.text)
+    print(title.text)
+    print(title["href"])
+    print()
+
+    rank_num+=1
+
+# driver.quit()
